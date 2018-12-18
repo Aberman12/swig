ae4eca801a99e8da0c5539f2da3c64c05b0c7754
Paulo Ramos <PauloRamos64@gmail.com>
2018-12-17 18:26:23 -0800

    Create App Header/Footer; Add to App.js

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Container, Content } from "native-base";
 
 import Counter from "./components/Counter";
+import AppHeader from "./components/AppHeader";
+import AppFooter from "./components/AppFooter";
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
+        <AppHeader />
         <Content>
           <Counter />
         </Content>
+        <AppFooter />
       </Container>
     );
   }
