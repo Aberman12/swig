099223c56a19ae2840718af481c167f8020e9c98
Paulo Ramos <PauloRamos64@gmail.com>
2018-12-17 18:16:37 -0800

    Add native-base and refactor App.js

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { styles } from "./styles";
-import Title from "./components/Title";
+import { Container, Content } from "native-base";
+
 import Counter from "./components/Counter";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.app}>
-        <Title />
-        <Counter />
-      </View>
+      <Container>
+        <Content>
+          <Counter />
+        </Content>
+      </Container>
     );
   }
 }
